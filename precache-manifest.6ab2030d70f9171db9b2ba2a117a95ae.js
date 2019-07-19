2032b57dcc2860de4703c895fd84402db3b1e2eb
Andrey <maksimov.andrew@gmail.com>
2019-07-19 10:34:43 +0300

    Updates

---
+++ precache-manifest.6ab2030d70f9171db9b2ba2a117a95ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63175896813627dc8606a96ce730b0a9",
+    "url": "/city-game/index.html"
+  },
+  {
+    "revision": "b870f3b643b9c4e0fdb6",
+    "url": "/city-game/static/css/main.6aa8aef3.chunk.css"
+  },
+  {
+    "revision": "43d4c7cc65be73410110",
+    "url": "/city-game/static/js/2.7bd98c86.chunk.js"
+  },
+  {
+    "revision": "b870f3b643b9c4e0fdb6",
+    "url": "/city-game/static/js/main.b3383531.chunk.js"
+  },
+  {
+    "revision": "363ebaf164003d0010f9",
+    "url": "/city-game/static/js/runtime~main.a4debc96.js"
+  }
+]);
