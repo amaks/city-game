90d011c6d045ec03ec24d5b40825835ded94f565
Andrey <maksimov.andrew@gmail.com>
2019-07-22 10:04:52 +0300

    remove chosen action quantity

--- src/components/chosen_action.js
+++ src/components/chosen_action.js
@@ -33,7 +33,7 @@ class ChosenAction extends React.Component {
                 &nbsp;
                 <a href='' data-item={this.actions.indexOf(item)} onClick={this.handleDelete}>x</a>
                 <br/>
-                <small><i>{getActionTypeName(this.actions.indexOf(item))} - 1</i></small>
+                <small><i>{getActionTypeName(this.actions.indexOf(item))}</i></small>
               </li>
               ))}
               <li className='list-group-item'>Осталось: { getBudget() } EUR</li>
